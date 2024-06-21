5817372666b842cd7aa50bdca0dcaa6747e55805
Eddy Pan <eddydotpan@gmail.com>
2024-06-21 14:42:28 -0400

    Finish preparing script for testing base TrOCR model

--- test-trocr.py
+++ test-trocr.py
@@ -1,1 +1,17 @@
+"""
+Script for TrOCR on test images to evaluate accuracy on my handwriting.
+"""
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
 
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
+filename = "attachments/eddy-red-pen-handwriting.jpeg"
+
+
+image = Image.open(filename).convert("RGB")
+pixel_values = processor(image, return_tensors="pt").pixel_values
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(f"Output:\n{generated_text}")
